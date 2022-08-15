5743eae8e79e56fa1066295f53c09377ecafc16a
Putu Bagus Angga Darmawan <bagusangga2301@gmail.com>
2022-08-15 11:21:39 +0800

    update 1.1

--- dist/js/script.js
+++ dist/js/script.js
@@ -35,3 +35,79 @@ darkToggle.addEventListener('click', function () {
         html.classList.remove('dark');
     }
 });
+
+// SLIDE IMAGE DETAIL PRODUK
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    spacebetween: 100,
+    autoplay: {
+        delay: 3000,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+// filter
+var openmodal = document.querySelectorAll('.modal-open')
+for (var i = 0; i < openmodal.length; i++) {
+  openmodal[i].addEventListener('click', function(event){
+    event.preventDefault()
+    toggleModal()
+  })
+}
+
+const overlay = document.querySelector('.modal-overlay')
+overlay.addEventListener('click', toggleModal)
+
+var closemodal = document.querySelectorAll('.modal-close')
+for (var i = 0; i < closemodal.length; i++) {
+  closemodal[i].addEventListener('click', toggleModal)
+}
+
+document.onkeydown = function(evt) {
+  evt = evt || window.event
+  var isEscape = false
+  if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc")
+  } else {
+    isEscape = (evt.keyCode === 27)
+  }
+  if (isEscape && document.body.classList.contains('modal-active')) {
+    toggleModal()
+  }
+};
+
+
+function toggleModal () {
+  const body = document.querySelector('body')
+  const modal = document.querySelector('.modal')
+  modal.classList.toggle('opacity-0')
+  modal.classList.toggle('pointer-events-none')
+  body.classList.toggle('modal-active')
+};
+
+
+// URUT BERDASARKAN
+const urutBerdasarkan = document.querySelector('#urut-berdasarkan');
+const dropUrutkan = document.querySelector('#drop-urutkan');
+
+hamburger.addEventListener('click', function () {
+    urutBerdasarkan.classList.toggle('urut-berdasarkan-active');
+    dropUrutkan.classList.toggle('hidden');
+});
+
